a3da1bc5cd605cc1ea972bb34e3897e76dfd7e54
felipegeneroso <felipegenerosobr@gmail.com>
2023-07-25 17:38:46 -0300

    Moto calculate inicio js

---
+++ script.js
@@ -0,0 +1,7 @@
+const calculateButton = document.querySelector(".calculate-button")
+
+function calculateValues(){
+    console.log("funcionou")
+}
+
+calculateButton.addEventListener("click", calculateValues)
