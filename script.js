c5b8940335a953496aa19c5808f00ccf886d2ee4
felipegeneroso <felipegenerosobr@gmail.com>
2023-07-26 09:23:24 -0300

    Codigo Funcionando

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const calculateButton = document.querySelector(".calculate-button")
 
 function calculateValues(){
-    console.log("funcionou")
+   
+    const kmIncialValue = document.querySelector("#km-inicial").value //pega valor km inicial
+    const kmFinalValue = document.querySelector("#km-final").value // pega valor km final
+    const entregaValue = document.querySelector("#entrega").value // pega valor entrega
+    const ganhoValue = document.querySelector("#ganho").value // valor dinheiro
+
+    const kmTotalFinalValue = document.querySelector(".km-total-final") //mapeado para colocar valor do KM total
+    const ganhoTotal = document.querySelector("#total-1") //mapeado para colocar valor em dinheiro
+    const kmPorEntrega = document.querySelector("#ganho-por-km") //mapeado para colocar valor ganho por km
+    const porEntregaValue = document.querySelector("#por-entrega") // mapeado para colocar valor r$ por tele
+
+
+    const kmTotal = kmFinalValue - kmIncialValue //variavel calculo do KM total
+    const ganhoPorKM = (ganhoValue / (kmFinalValue - kmIncialValue)) //variavel calcula de Ganho por KM
+    const ganhoTotalPorEntrega = ganhoValue / entregaValue //variavel calculo de R$ por entrega
+
+
+    kmTotalFinalValue.innerHTML = kmTotal
+    ganhoTotal.innerHTML = ganhoValue
+    kmPorEntrega.innerHTML = ganhoPorKM
+    porEntregaValue.innerHTML = ganhoTotalPorEntrega
+    
 }
 
 calculateButton.addEventListener("click", calculateValues)
